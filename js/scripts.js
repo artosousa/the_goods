6c292e420ade8b7d91dec9438922f5010c36ea26
MacBook <macbookpro@Macbook-Pro.local>
2019-08-18 18:33:04 -0400

    first goods commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const theGoods = document.getElementById('the-goods');
+const cakesCove = document.getElementById('cakes-cove');
+
+window.addEventListener("load", function() {
+    setTimeout(function(){
+        theGoods.classList.add("fade-in");
+        cakesCove.classList.add("hidden");
+        
+      }, 3000);
+});
